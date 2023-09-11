5a6f9aa4cd0374b2cbe6ed5f32b8323fc021891b
axelKeizoStahl <axelkeizo@gmail.com>
2023-09-11 18:36:05 -0400

    9/11/23

---
+++ CW#1.scm
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname CW#1) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;Task 1
+(/ (+ 2 3) 4)
+(/ 2 (+ 3 (* 4 5)))
+(+ (* 2 3) (/ 4 5) (* 6 7))
+;Task 2
+;(2+6)(4)(4)+2+3
+;(2*8)/(2+2)
---
+++ HW#1.scm
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname HW#1) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;Problem 1
+;(5+4)/(15-2)
+(/ (+ 5 4) (- 15 2))
+;Problem 2
+;(7+5sqrt(9))/((2^2)+(12/4))
+(/ (+ 7 (* 5 (sqrt 9))) (+ (expt 2 2) (/ 12 4)))
+;Problem 3
+;((4/8)+3)/((15/3)+(9/3))
+(/ (+ (/ 4 8) 3) (+ (/ 15 3) (/ 9 3)))
