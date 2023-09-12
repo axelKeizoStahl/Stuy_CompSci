153b2ea7af0f60b11972c59726a487f4a5cdcc9a
axelKeizoStahl <axelkeizo@gmail.com>
2023-09-12 17:27:22 -0400

    9/12/23

--- HW#1.scm
+++ HW#1.scm
@@ -1,5 +1,3 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname HW#1) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ;Problem 1
 ;(5+4)/(15-2)
@@ -9,4 +7,4 @@
 (/ (+ 7 (* 5 (sqrt 9))) (+ (expt 2 2) (/ 12 4)))
 ;Problem 3
 ;((4/8)+3)/((15/3)+(9/3))
-(/ (+ (/ 4 8) 3) (+ (/ 15 3) (/ 9 3)))
+(/ (+ (/ 4 8) 3) (- (/ 15 3) (/ 9 3)))
