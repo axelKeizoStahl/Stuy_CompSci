c3e4b2dc96c5da2358b955b9c0d0070881d1b0f0
axelKeizoStahl <axelkeizo@gmail.com>
2023-10-02 22:24:08 -0400

    Challenge#8 update

--- Challenge#8.scm
+++ Challenge#8.scm
@@ -2,3 +2,5 @@
 
 (define (magicPair a b)
   (or (and (or (= (modulo a 10) (quotient b 10)) (= (modulo b 10) (quotient a 10))) (= (+ (modulo b 10) (quotient a 10)) (+ (modulo a 10) (quotient b 10)))) (and (or (= (modulo a 10) (modulo b 10)) (= (quotient a 10) (quotient b 10))) (= (+ (modulo a 10) (modulo b 10)) (+ (quotient b 10) (quotient a 10))))))
+
+(define (makeChocolate sb bb goal) (if (and (> (modulo goal 5) 0) (<= (modulo goal 5) sb)) (modulo goal 5) -1))
